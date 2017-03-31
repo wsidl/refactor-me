2b0269e5361672b88b92aff6b7c6857cd17357c0
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2017-03-31 14:01:54 +0300

    setdefault() is more readable although has less performance than if-statement variant

--- refactor_me.py
+++ refactor_me.py
@@ -9,8 +9,7 @@ def summarize_expenses(min_amount, input):
     expenses = {}
     for expense in input:
         if expense.amount >= min_amount:
-            if not expense.type_ in expenses:
-                expenses[expense.type_] = 0
+            expenses.setdefault(expense.type_, 0)
             expenses[expense.type_] = expenses[expense.type_] + expense.amount
 
     for (expense, amount) in sorted(expenses.items(), key=lambda e: e[1], reverse=False):
