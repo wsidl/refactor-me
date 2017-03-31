1036c091cb70ef110b4e56702bdc012c8a110336
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2017-03-31 13:22:03 +0300

    Original code

---
+++ README.rst
@@ -0,0 +1,1 @@
+refactor_me.py is expected to contain Python 3.5.x code
---
+++ refactor_me.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+foo = []
+
+from .models import Expense
+from collections import namedtuple
+
+MyExpense = namedtuple('F', ['type_', 'amount'])
+
+# test data
+foo.append(MyExpense('food', 4))
+foo.append(MyExpense('food', 3))
+foo.append(MyExpense('car', 3))
+foo.append(MyExpense('dog', 1))
+
+
+def summarizeExpenses(min_amount, input):
+    expenses = {}
+    for expense in input:
+        if expense.amount >= min_amount:
+            if not expense.type_ in expenses:
+                expenses[expense.type_] = 0
+            expenses[expense.type_] = expenses[expense.type_] + expense.amount
+
+    for (expense, amount) in sorted(expenses.items(), key=lambda e: e[1], reverse=False):
+        print expense.type_, amount
+
+summarizeExpenses(2, foo)
