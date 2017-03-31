b8af23a1f7def78b995042f67ced6cbe979306ea
Dmitry Mugtasimov <dmugtasimov@gmail.com>
2017-03-31 14:20:30 +0300

    Removed side effects from `sum_expenses()` now it can be reused for summation of expenses without printing

--- refactor_me.py
+++ refactor_me.py
@@ -10,12 +10,16 @@ def sum_expenses(expenses, min_amount=0):
     for expense in expenses:
         if expense.amount >= min_amount:
             aggregated_expenses[expense.type_] += expense.amount
+    return aggregated_expenses
 
-    for expense, amount in sorted(aggregated_expenses.items(),
+
+def print_expenses(expenses):
+    for expense, amount in sorted(expenses.items(),
                                   key=lambda x: x[1], reverse=False):
         print(expense, amount)
+
 
 # test data
 # TODO(dmu) HIGH: Use static fixtures and dynamic fixture framework instead
 foo = [MyExpense('food', 4), MyExpense('food', 3), MyExpense('car', 3), MyExpense('dog', 1)]
-sum_expenses(foo, 2)
+print_expenses(sum_expenses(foo, 2))
