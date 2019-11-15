718d0b4daa9b72583ddcb541408490306eb7cc72
wsidl <will.siddall@gmail.com>
2019-11-15 08:10:09 -0800

    Re-formatting line lengths

--- refactor_me.py
+++ refactor_me.py
@@ -30,7 +30,8 @@ def format_dict(input_dict, write_out=None):
 
     Args:
         input_dict (dict): Values to output to command line
-        write_out (io.IOBase): Output object that receives the formatted string, defaults writing to terminal
+        write_out (io.IOBase): Output object that receives the formatted string, defaults
+            writing to terminal
     """
     key_size = max([len(key) for key in input_dict.keys()])
     (write_out or stdout).write(
@@ -43,5 +44,7 @@ def format_dict(input_dict, write_out=None):
 
 if __name__ == '__main__':
     # TODO(dmu) HIGH: Use static fixtures and dynamic fixture framework instead
-    test_expenses = (Expense('food', 4), Expense('food', 3), Expense('car', 3), Expense('dog', 1))
+    test_expenses = (
+        Expense('food', 4), Expense('food', 3), Expense('car', 3), Expense('dog', 1)
+    )
     format_dict(sum_expenses(test_expenses, 2))
